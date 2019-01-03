ffd86bdb3ed321464173324d174e1a2f3610e993
Gal Bumbar <gal.bumbar@gmail.com>
2019-01-03 22:22:56 +0100

    added logging

--- api/src/main/java/api/v1/SubscriptionsApplication.java
+++ api/src/main/java/api/v1/SubscriptionsApplication.java
@@ -2,11 +2,12 @@ package api.v1;
 
 
 import com.kumuluz.ee.discovery.annotations.RegisterService;
-
+import com.kumuluz.ee.logs.cdi.Log;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @RegisterService
+@Log
 @ApplicationPath("/api/v1")
 public class SubscriptionsApplication extends Application{
 }
